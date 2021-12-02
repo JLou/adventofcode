06b4fc3dfd7960816eb2805d12b50150414b2dd6
Jean-Lou Piermé <jeanlou.pierme@gmail.com>
2021-12-02 09:12:12 +0100

    day1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿var lines = File.ReadAllLines("./input/1.txt").Select(l => int.Parse(l)).ToList();
+
+
+int CountIncreases(IEnumerable<int> l)
+{
+
+    var prev = int.MaxValue;
+    var count = 0;
+
+    foreach (var line in l)
+    {
+        if (line > prev)
+        {
+            count++;
+        }
+        prev = line;
+    }
+
+    return count;
+}
+
+int[] groups = new int[lines.Count() - 2];
+
+
+for (int i = 0; i < lines.Count(); i++)
+{
+    if (i < lines.Count() - 2) groups[i] += lines[i];
+    if (i - 1 >= 0 && i - 1 < lines.Count() - 2) groups[i - 1] += lines[i];
+    if (i - 2 >= 0 && i < lines.Count()) groups[i - 2] += lines[i];
+}
+Console.WriteLine(CountIncreases(lines));
+Console.WriteLine(CountIncreases(groups));
+
