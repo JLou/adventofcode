b11db240dfa571f18b585302ed00db28f7bf8b1e
Jean-Lou Piermé <jeanlou.pierme@gmail.com>
2021-12-10 10:42:04 +0100

    day10

--- Day09.cs
+++ Day09.cs
@@ -1,10 +1,57 @@
-namespace aoc
+namespace aoc;
+
+public class Day09 : IDay
 {
-    public class Day09 : IDay
+    public (string, string) Compute(string[] input)
     {
-        public (string, string) Compute(string[] input)
+        var map = input.Select(line => line.Select(x => int.Parse(x.ToString())).ToArray()).ToArray();
+
+        int xMax = map[0].Length;
+        int yMax = map.Length;
+
+        var lowPoints = new List<int>();
+        var lowPointsCoord = new List<(int x, int y)>();
+        for (int y = 0; y < yMax; y++)
         {
-            return ("", "");
+            for (int x = 0; x < xMax; x++)
+            {
+                if (GetNeighbours(x, y, xMax, yMax).All(coord => map[coord.y][coord.x] > map[y][x]))
+                {
+                    lowPoints.Add(map[y][x]);
+                    lowPointsCoord.Add((x, y));
+                }
+            }
         }
+
+        var basins = new List<int>();
+
+        foreach (var (x, y) in lowPointsCoord)
+        {
+            var basin = new HashSet<(int, int)> { (x, y) };
+            (int x, int y)[] higherNeighbours = new (int x, int y)[] { (x, y) };
+            do
+            {
+                higherNeighbours = higherNeighbours
+                    .SelectMany(coord => GetNeighbours(coord.x, coord.y, xMax, yMax).Where(neighCoord =>
+                           map[neighCoord.y][neighCoord.x] > map[coord.y][coord.x]
+                        && map[neighCoord.y][neighCoord.x] < 9))
+                    .ToArray();
+                ;
+                basin = new(basin.Union(higherNeighbours));
+
+            } while (higherNeighbours.Length > 0);
+            basins.Add(basin.Count);
+        }
+
+        var risk1 = lowPoints.Select(x => x + 1).Sum();
+        var risk2 = basins.OrderByDescending(x => x).Take(3).Aggregate(1, (acc, next) => acc * next);
+        return ($"{risk1}", $"{risk2}");
+    }
+    private IEnumerable<(int x, int y)> GetNeighbours(int x, int y, int xMax, int yMax)
+    {
+        if (x > 0) yield return (x - 1, y);
+        if (y > 0) yield return (x, y - 1);
+        if (x < xMax - 1) yield return (x + 1, y);
+        if (y < yMax - 1) yield return (x, y + 1);
     }
 }
--- Day10.cs
+++ Day10.cs
@@ -4,7 +4,88 @@ namespace aoc
     {
         public (string, string) Compute(string[] input)
         {
-            return ("", "");
+
+            var illegals = new List<char>();
+            var charMap = new Dictionary<char, char>
+            {
+                ['{'] = '}',
+                ['<'] = '>',
+                ['('] = ')',
+                ['['] = ']',
+            };
+
+            var scoremapMissing = new Dictionary<char, int>
+            {
+                [')'] = 1,
+                [']'] = 2,
+                ['}'] = 3,
+                ['>'] = 4
+            };
+
+            var scores2 = new List<long>();
+            foreach (var line in input)
+            {
+                var stack = new Stack<char>();
+
+                var c = FindIllegalChar(stack, line);
+                var score = 0L;
+                if (c.HasValue) { illegals.Add(c.Value); }
+                else
+                {
+                    while (stack.Count > 0)
+                    {
+                        var ch = stack.Pop();
+                        var missingCh = charMap[ch];
+                        score = score * 5 + scoremapMissing[missingCh];
+                    }
+                }
+
+            }
+
+            var scoremap = new Dictionary<char, int>
+            {
+                [')'] = 3,
+                [']'] = 57,
+                ['}'] = 1197,
+                ['>'] = 25137
+            };
+
+
+            return ($"{illegals.Select(c => scoremap[c]).Sum()}", $"{scores2.OrderBy(x => x).ElementAt(scores2.Count / 2)}");
+        }
+
+        private static char? FindIllegalChar(Stack<char> stack, string line)
+        {
+            foreach (var c in line)
+            {
+                switch (c)
+                {
+                    case '(':
+                    case '{':
+                    case '[':
+                    case '<':
+                        stack.Push(c);
+                        break;
+                    case ')':
+                        if (stack.Pop() != '(') return c;
+                        break;
+
+                    case '}':
+                        if (stack.Pop() != '{') return c;
+                        break;
+
+                    case ']':
+                        if (stack.Pop() != '[') return c;
+                        break;
+
+                    case '>':
+                        if (stack.Pop() != '<') return c;
+                        break;
+                    default:
+                        throw new Exception();
+                }
+            }
+            return null;
         }
     }
 }
