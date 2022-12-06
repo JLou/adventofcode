89731fb502e0c866b8180b3587724c73a627abff
PIERMÉ Jean-Lou <A459HB@login.axa>
2022-12-06 08:55:07 +0100

    2022: day06

--- 2022/06.py
+++ 2022/06.py
@@ -1,2 +1,16 @@
 with open('inputs/06') as f:
     lines = f.readlines()
+
+def findIndex(input, n):
+    for i in range(n, len(input)):
+        if len(set(input[i-n:i])) == n:
+            return i
+
+def part1(input):
+        return findIndex(input, 4)
+
+def part2(input):
+    return findIndex(input, 14)
+
+print(part1(lines[0]))
+print(part2(lines[0]))
