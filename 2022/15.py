f4317edf43849c1afc501449598ae7e1c7fcb7f6
PIERMÉ Jean-Lou <A459HB@login.axa>
2022-12-16 17:27:35 +0100

    2022: day 15 + day 16p1

--- 2022/15.py
+++ 2022/15.py
@@ -1,2 +1,103 @@
+import re
+from typing import NamedTuple, Set, Tuple
 with open('inputs/15') as f:
     lines = f.readlines()
+
+sample = """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3""".splitlines()
+
+
+def part1(lines, row):
+    beacon_pos = []
+    sensor_pos = []
+    most_left = 0
+    most_right = 0
+
+    for l in lines:
+        m = re.search(
+            'Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)', l)
+        sx = int(m.group(1))
+        sy = int(m.group(2))
+        bx = int(m.group(3))
+        by = int(m.group(4))
+
+        d = abs(sx-bx) + abs(sy-by)
+        beacon_pos.append((bx, by))
+        sensor_pos.append((sx, sy, d))
+        most_left = min(sx - d, most_left)
+        most_right = max(sx + d, most_right)
+
+    visible = []
+    for x in range(most_left, most_right+1):
+        y = row
+
+        for sx, sy, d in sensor_pos:
+            if abs(sx-x) + abs(sy-y) <= d:
+                if (x, y) not in beacon_pos:
+                    visible.append((x, y))
+                break
+
+    print(len(visible))
+
+
+class Line(NamedTuple):
+    m: float
+    b: float
+
+
+class Point(NamedTuple):
+    x: int
+    y: int
+
+
+def line_intersection(line1: Line, line2: Line):
+    x = (line1.b - line2.b) / (line2.m - line1.m)
+    y = line1.m * x + line1.b
+    return Point(round(x), round(y))
+
+
+def part2(data, minv, maxv):
+    lines: Set[Line] = set()
+    line_pairs: Set[Tuple[Line, Line]] = set()
+    for l in data:
+        sensor_x, sensor_y, beacon_x, beacon_y = map(
+            int, re.findall("-?\d+", l))
+
+        d = abs(sensor_x-beacon_x) + abs(sensor_y-beacon_y)
+        points = [Point(sensor_x + d, sensor_y), Point(sensor_x - d, sensor_y),
+                  Point(sensor_x, sensor_y + d), Point(sensor_x, sensor_y - d)]
+
+        for a, b in [(a, b) for i, a in enumerate(points) for b in points[i+1:]]:
+            if not a.x == b.x and not a.y == b.y:
+                m = (a.x - b.x) / (a.y - b.y)
+                b = a.y - m * a.x
+                lines.add(Line(m, b))
+
+        for line1 in lines:
+            for line2 in lines:
+                if line1 is not line2:
+                    def by_b(x): return x.b
+                    if line1.m == line2.m and abs(line1.b - line2.b) == 2:
+                        line_pairs.add(
+                            (min(line1, line2, key=by_b), max(line1, line2, key=by_b)))
+
+    pairs = sorted(line_pairs, key=lambda x: x[0].b)
+    print(pairs)
+    intersection = line_intersection(pairs[0][1], pairs[1][0])
+    print((intersection.x + 1) * 4_000_000 + intersection.y)
+
+
+part1(lines, 2000000)
+part2(lines, 0, 4000000)
--- 2022/16.py
+++ 2022/16.py
@@ -1,2 +1,98 @@
+import functools
+import re
+from typing import List, NamedTuple
+from collections import deque
+from functools import cache
 with open('inputs/16') as f:
     lines = f.readlines()
+
+sample = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II""".splitlines()
+
+
+class Node(NamedTuple):
+    name: str
+    flowRate: int
+    neighbours: List[str]
+
+
+def compute_graph(data: List[str]):
+    regexp = 'Valve (.+) has flow rate=(\d+); tunnels? leads? to valves? (.+)'
+    graph: dict[str, Node] = dict()
+    for l in data:
+        m = re.match(regexp, l)
+        valveInput = m.group(1)
+        flowRate = int(m.group(2))
+        neighbours = m.group(3).split(', ')
+        graph[valveInput] = Node(valveInput, flowRate, neighbours)
+
+    return graph
+
+
+graph: dict[str, Node] = dict()
+valves = []
+
+
+@cache
+def dijkstra(start_node: str):
+    start = graph[start_node]
+    distance = dict()
+    for k in graph.keys():
+        distance[k] = 1e7
+    distance[start.name] = 0
+    q = deque()
+    for n in start.neighbours:
+        q.append((n, start.name))
+    visited = set(start.name)
+
+    while len(q) > 0:
+
+        curr, parent = q.popleft()
+        distance[curr] = min(distance[parent] + 1, distance[curr])
+        if curr not in visited:
+            visited.add(curr)
+            for n in graph[curr].neighbours:
+                q.append((n, curr))
+
+    return distance
+
+
+def compute_points(l: List[str]):
+    global graph
+    if len(l) == 0:
+        return 0
+    s = sum(map(lambda x: graph[x].flowRate, l))
+    return s * len(l)
+
+
+@functools.cache
+def compute_best(start, time, remaining):
+    d = dijkstra(start)
+    if time < 1:
+        return 0
+    flow = graph[start].flowRate * (time)
+    return max([flow + compute_best(v, time - d[v] - 1,
+                                    remaining-{v}) for v in remaining if d[v] < time] + [0])
+
+
+def part1(data):
+    global graph, valves
+    graph = compute_graph(data)
+    l = list(graph.keys())
+
+    v = frozenset([x for x in l if graph[x].flowRate > 0])
+    result = (compute_best("AA", 30, v))
+    print(result)
+
+
+if __name__ == "__main__":
+    # part1(sample)
+    part1(lines)
