8f204dca255683efbd85159553a6255296613daa
PIERMÉ Jean-Lou <A459HB@login.axa>
2022-12-23 10:41:29 +0100

    2022: day 21 & 22

--- 2022/22.py
+++ 2022/22.py
@@ -1,2 +1,257 @@
+import re
+from typing import List, NamedTuple, Tuple
+
 with open('inputs/22') as f:
-    lines = f.readlines()
+    lines = [x.ljust(150, ' ') for x in f.read().splitlines()]
+
+sample = """        ...#
+        .#..
+        #...
+        ....
+...#.......#
+........#...
+..#....#....
+..........#.
+        ...#....
+        .....#..
+        .#......
+        ......#.
+
+10R5L5R10L4R5L5
+""".splitlines()
+
+
+class Instruction(NamedTuple):
+    dist: int
+    turn: str
+
+
+def parse(input):
+    instruction_line = input[-1]
+    match = re.findall("\d+[RL]", instruction_line)
+    instructions = [Instruction(int(x[:-1]), x[-1]) for x in match]
+
+    puzzle_map = input[:-2]
+
+    return (instructions, puzzle_map)
+
+
+def parse_cube(input):
+    faces = [[] for x in range(7)]
+    for l in input[:50]:
+        faces[1].append(l[50:100])
+        faces[2].append(l[100:])
+    for l in input[50:100]:
+        faces[3].append(l[50:100])
+    for l in input[100:150]:
+        faces[5].append(l[0:50])
+        faces[4].append(l[50:100])
+    for l in input[150:200]:
+        faces[6].append(l[0:50])
+
+    instruction_line = input[-1]
+    match = re.findall("\d+[RL]", instruction_line)
+    instructions = [Instruction(int(x[:-1]), x[-1]) for x in match]
+    instructions.append(Instruction(int(instruction_line[-2:]), ''))
+    return instructions, faces
+
+
+def find_next_tile(map, x, y, dir):
+    dx, dy = dir
+
+    next_x = x+dx
+    next_y = y+dy
+
+    # warp down
+    if next_y >= len(map):
+        for j in range(next_y):
+            if map[j][next_x] != ' ':
+                return (next_x, j)
+    # warp up
+    if next_y < 0:
+        for i in range(len(map) - 1, 0, -1):
+            if map[i][next_x] != ' ':
+                return (next_x, i)
+
+    # wrap right
+    if next_x >= len(map[next_y]):
+        for i in range(next_x):
+            if map[next_y][i] != ' ':
+                return (i, next_y)
+    # warp left
+    if next_x < 0:
+        for i in range(len(map[next_y]) - 1, 0, -1):
+            if map[next_y][i] != ' ':
+                return (i, next_y)
+
+    while map[next_y][next_x] == ' ':
+        next_x, next_y = find_next_tile(map, next_x, next_y, dir)
+
+    return (next_x, next_y)
+
+
+RIGHT = 0
+DOWN = 1
+LEFT = 2
+UP = 3
+
+
+def find_next_tile_cube(cube: List[List[str]], face: int, x: int, y: int, dir: Tuple[int, int], currentDir: int):
+    dx, dy = dir
+
+    next_x = x+dx
+    next_y = y+dy
+
+    curr_zone = cube[face]
+
+    if next_y >= len(curr_zone):
+        if face == 1:
+            return (3, next_x, 0, DOWN)
+        elif face == 2:
+            return (3, 49, next_x, LEFT)
+        elif face == 3:
+            return (4, next_x, 0, DOWN)
+        elif face == 4:
+            return (6, 49, next_x, LEFT)
+        elif face == 5:
+            return (6, next_x, 0, DOWN)
+        else:
+            return (2, next_x, 0, DOWN)
+    # warp up
+    if next_y < 0:
+        if face == 1:
+            return (6, 0, next_x, RIGHT)
+        elif face == 2:
+            return (6, next_x, 49, UP)
+        elif face == 3:
+            return (1, next_x, 49, UP)
+        elif face == 4:
+            return (3, next_x, 49, UP)
+        elif face == 5:
+            return (3, 0, next_x, RIGHT)
+        else:
+            return (5, next_x, 49, UP)
+
+    # wrap right
+    if next_x >= len(curr_zone[0]):
+        if face == 1:
+            return (2, 0, next_y, RIGHT)
+        elif face == 2:
+            return (4, 49, 49-next_y, LEFT)
+        elif face == 3:
+            return (2, next_y, 49, UP)
+        elif face == 4:
+            return (2, 49, 49-next_y, LEFT)
+        elif face == 5:
+            return (4, 0, next_y, RIGHT)
+        else:
+            return (4, next_y, 49, UP)
+
+        # warp left
+    if next_x < 0:
+        if face == 1:
+            return (5, 0, 49-next_y, RIGHT)
+        elif face == 2:
+            return (1, 49, next_y, LEFT)
+        elif face == 3:
+            return (5, next_y, 0, DOWN)
+        elif face == 4:
+            return (5, 49, next_y, LEFT)
+        elif face == 5:
+            return (1, 0, 49-next_y, RIGHT)
+        else:
+            return (1, next_y, 0, DOWN)
+
+    return face, next_x, next_y, currentDir
+
+
+def run(instructions: List[Instruction], map: List[str]):
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    y = 0
+    x = map[y].index('.')
+    direction = 0
+
+    for dist, rotate in instructions:
+        for i in range(0, dist):
+            dx, dy = directions[direction]
+
+            next_x, next_y = find_next_tile(map, x, y, (dx, dy))
+            next_tile = map[next_y][next_x]
+
+            if next_tile == '#':
+                break
+            else:
+                x, y = next_x, next_y
+        direction += 1 if rotate == 'R' else -1
+        direction %= 4
+
+    return x, y, direction
+
+
+def print_cube(cube):
+    for i in range(50):
+        print((" " * 50) + "".join(cube[1][i]) + "".join(cube[2][i]))
+    for i in range(50):
+        print((" " * 50) + "".join(cube[3][i]))
+    for i in range(50):
+        print("".join(cube[5][i]) + "".join(cube[4][i]))
+    for i in range(50):
+        print("".join(cube[6][i]))
+
+
+def run2(instructions: List[Instruction], cube: List[str]):
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    y = 0
+    face = 1
+    x = cube[face][y].index('.')
+    direction = RIGHT
+
+    copy = [[[c for c in line] for line in face] for face in cube]
+    chars = ['>', 'V', '<', '^']
+    copy[face][y][x] = chars[direction]
+    u = 0
+    for dist, rotate in instructions:
+        for i in range(0, dist):
+            dx, dy = directions[direction]
+
+            next_face, next_x, next_y, next_dir = find_next_tile_cube(
+                cube, face, x, y, (dx, dy), direction)
+            next_tile = cube[next_face][next_y][next_x]
+
+            if next_tile == '#':
+                break
+            else:
+                face, direction, x, y = next_face, next_dir, next_x, next_y
+                copy[face][y][x] = chars[direction]
+        if rotate != '':
+            direction += 1 if rotate == 'R' else -1
+            direction %= 4
+        u += 1
+
+        # if u % 20 == 0:
+        #     print_cube(copy)
+        #     print(" ")
+    # for i, f in enumerate(copy):
+    #     print(f"face {i}")
+    #     for l in f:
+    #         print("".join(l))
+    #     print("")
+
+    return face, x, y, direction
+
+
+def part1(input):
+    instructions, map = parse(input)
+    x, y, dir = run(instructions, map)
+    print((y+1) * 1000 + (x+1) * 4 + dir)
+
+
+def part2(input):
+    instructions, cube = parse_cube(input)
+    face, x, y, direction = run2(instructions, cube)
+
+    print(face, x, y, direction)
+    print((y + 1) * 1000 + (x + 100 + 1) * 4 + direction)
+
+
+part2(lines)
--- 2022/23.py
+++ 2022/23.py
@@ -1,2 +1,103 @@
+from collections import defaultdict, deque
+from typing import Tuple
+import itertools
+
+
 with open('inputs/23') as f:
-    lines = f.readlines()
+    lines = f.read().splitlines()
+
+sample = """....#..
+..###.#
+#...#.#
+.#...##
+#.###..
+##.#.##
+.#..#..""".splitlines()
+
+
+def parse(lines):
+    elves = set()
+
+    for i, l in enumerate(lines):
+        for j, c in enumerate(l):
+            if c == '#':
+                elves.add((j, i))
+
+    return elves
+
+
+def print_map(elves):
+    for y in range(-2, 9):
+        l = ""
+        for x in range(-3, 11):
+            if (x, y) in elves:
+                l += "#"
+            else:
+                l += '.'
+        print(l)
+
+
+def run(elves: set, part2=False):
+    directions = deque([[(-1, -1), (0, -1), (1, -1)],
+                       [(-1, 1), (0, 1), (1, 1)], [(-1, -1), (-1, 0), (-1, 1)], [(1, -1), (1, 0), (1, 1)]])
+
+    i = 1
+    while True:
+        proposed_moves: defaultdict[Tuple, list] = defaultdict(list)
+        for (x, y) in elves:
+            u = sum([1 for a, b in itertools.product(
+                [-1, 0, 1], repeat=2) if (x+a, y+b) in elves])
+            if u > 1:
+                for test_dir in directions:
+                    in_the_way = False
+                    for dx, dy in test_dir:
+                        if (x+dx, y+dy) in elves:
+                            in_the_way = True
+                            continue
+                    if not in_the_way:
+                        proposed_moves[(x+test_dir[1][0], y+test_dir[1]
+                                        [1])].append((x, y))
+                        break
+        has_moved = False
+        for next_move in proposed_moves:
+            if len(proposed_moves[next_move]) == 1:
+                has_moved = True
+                elves.add(next_move)
+                elves.remove(proposed_moves[next_move][0])
+
+        directions.rotate(-1)
+
+        # print(f'Round {i+1}')
+        # print_map(elves)
+        # print(f'{len(elves)} elves left')
+        # print("")
+        if i == 10:
+            min_x = min_y = max_x = max_y = 0
+            for (x, y) in elves:
+                min_x = min(x, min_x)
+                min_y = min(y, min_y)
+                max_x = max(x, max_x)
+                max_y = max(y, max_y)
+            print((max_x + 1 - min_x) * (max_y + 1 - min_y) - len(elves))
+
+        if not has_moved or (not part2 and i == 10):
+            break
+        i += 1
+
+    print('Finished after round=')
+    print(i)
+    return elves
+
+
+def part1(lines):
+    elves = parse(lines)
+    run(elves)
+
+
+def part2(lines):
+    elves = parse(lines)
+    run(elves, True)
+
+
+# part1(lines)
+part2(lines)
