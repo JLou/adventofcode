5ab17dfa60ff461f8d1db592ff704306998f7de6
JLou <jlou.xd@gmail.com>
2023-12-10 19:50:09 +0100

    2023/08 & 2023/09

--- 2023/08.py
+++ 2023/08.py
@@ -1,2 +1,68 @@
+import math
+import re
+from collections import deque
+
 with open("./inputs/08", 'r') as f:
-    lines = f.read().splitlines()
+    directions_str, nodes = f.read().split('\n\n')
+
+# directions_str, nodes = """LR
+
+# 11A = (11B, XXX)
+# 11B = (XXX, 11Z)
+# 11Z = (11B, XXX)
+# 22A = (22B, XXX)
+# 22B = (22C, 22C)
+# 22C = (22Z, 22Z)
+# 22Z = (22B, 22B)
+# XXX = (XXX, XXX)""".split('\n\n')
+
+node_pattern = re.compile(r'(\w{3}) = \((\w{3}), (\w{3})\)')
+
+
+class Node:
+    def __init__(self, input_str: str) -> None:
+        name, left, right = re.findall(node_pattern, input_str)[0]
+        self.name = name
+        self.left = left
+        self.right = right
+
+    def __repr__(self) -> str:
+        return self.name + " = (" + self.left + ", " + self.right + ")"
+
+
+node_dict: "dict[str, Node]" = {}
+
+
+def move(start_pos, instruction) -> str:
+    return node_dict[start_pos].left if instruction == 'L' else node_dict[start_pos].right
+
+
+for l in nodes.splitlines():
+    node = Node(l)
+    node_dict[node.name] = node
+
+directions = deque(directions_str)
+
+position = "AAA"
+
+
+def count(start, is_part1=False):
+    directions_copy = deque(directions_str)
+    count = 0
+    while (start != "ZZZ" if is_part1 else not start.endswith("Z")):
+        count += 1
+        current_dir = directions_copy[0]
+        start = move(start_pos=start, instruction=current_dir)
+        directions_copy.rotate(-1)
+
+    return count
+
+
+print("PART 1:", count("AAA", True))
+
+
+start_nodes = list(filter(lambda x: x.endswith("A"), node_dict.keys()))
+counts = [count(x) for x in start_nodes]
+ppcm = math.lcm(*counts)
+
+print("Part 2:", ppcm)
--- 2023/09.py
+++ 2023/09.py
@@ -1,2 +1,39 @@
+sample = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
 with open("./inputs/09", 'r') as f:
-    lines = f.read().splitlines()
+    lines = [list(map(int, l.split(" "))) for l in f.read().splitlines()]
+
+# lines = [list(map(int, l.split(" "))) for l in sample.splitlines()]
+
+
+def find_last(seq: list[int]):
+    new_seq = []
+    for i in range(len(seq) - 1):
+        new_seq.append(seq[i+1] - seq[i])
+
+    if (all(x == 0 for x in new_seq)):
+        return seq[-1]
+    else:
+        return find_last(new_seq) + seq[-1]
+
+
+def find_first(seq: list[int]):
+    new_seq = []
+    for i in range(len(seq) - 1):
+        new_seq.append(seq[i+1] - seq[i])
+
+    if (all(x == 0 for x in new_seq)):
+        return seq[0]
+    else:
+        return seq[0] - find_first(new_seq)
+
+
+total = 0
+for line in lines:
+    v = find_last(line)
+    total += v
+
+print("PART 1:", total)
+print("PART 2:", sum([find_first(line) for line in lines]))
