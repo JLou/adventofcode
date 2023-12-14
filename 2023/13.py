869298cc0ff3c8c717b301a18bfcf2660a92e86c
JLou <jeanlou.pierme@gmail.com>
2023-12-14 09:28:39 +0100

    2023/13 part2

--- 2023/13.py
+++ 2023/13.py
@@ -2,7 +2,11 @@ with open("./inputs/13", 'r') as f:
     patterns = [block.splitlines() for block in f.read().split('\n\n')]
 
 
-def solve(pattern: list[str]) -> int:
+def count_diff(line1, line2):
+    return sum(1 for a, b in zip(line1, line2) if a != b)
+
+
+def find_mirror(pattern, factor):
     for i, line in enumerate(pattern[:-1]):
         if pattern[i+1] == line:
             to_lookup = min(i + 1, len(pattern) - (i+1))
@@ -12,21 +16,40 @@ def solve(pattern: list[str]) -> int:
                     is_mirrored = False
                     break
             if is_mirrored:
-                return (i + 1) * 100
+                return (i + 1) * factor
+    return None
 
+
+def solve(pattern: list[str]) -> int:
+    res = find_mirror(pattern, 100)
+    if res != None:
+        return res
     rotated = [''.join(x) for x in zip(*pattern[::-1])]
-    for i, line in enumerate(rotated[:-1]):
-        if rotated[i+1] == line:
-            to_lookup = min(i + 1, len(rotated) - (i+1))
+    return find_mirror(rotated, 1)
+
+
+def find_mirror_smudged(pattern, factor):
+    for i, line in enumerate(pattern[:-1]):
+        diff = count_diff(pattern[i+1], line)
+        if diff < 2:
+            to_lookup = min(i + 1, len(pattern) - (i+1))
             is_mirrored = True
             for j in range(1, to_lookup):
-                if rotated[i-j] != rotated[i+1+j]:
+                diff += count_diff(pattern[i-j], pattern[i+1+j])
+                if diff > 1:
                     is_mirrored = False
                     break
-            if is_mirrored:
-                return (i + 1)
+            if diff > 0 and is_mirrored:
+                return (i + 1) * factor
+    return None
 
-    return 0
+
+def solve_2(pattern: list[str]) -> int:
+    res = find_mirror_smudged(pattern, 100)
+    if res != None:
+        return res
+    rotated = [''.join(x) for x in zip(*pattern[::-1])]
+    return find_mirror_smudged(rotated, 1)
 
 
 sample1 = """#.##..##.
@@ -47,5 +70,8 @@ sample2 = """#...##..#
 
 assert (solve(sample2) == 400)
 assert (solve(sample1) == 5)
+assert (solve_2(sample1) == 300)
+assert (solve_2(sample2) == 100)
 
 print(sum(solve(block) for block in patterns))
+print(sum(solve_2(block) for block in patterns))
