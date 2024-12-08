f847e74eff3ff87a73971ba900d5f454acd46f84
JLou <jlou.xd@gmail.com>
2024-12-08 17:55:57 +0100

    setup 2024 stubs

---
+++ 2024/09.py
@@ -0,0 +1,17 @@
+def solve(input):
+    s = 0
+    s2 = 0
+
+    return s, s2
+
+
+test_input = """"""
+
+assert solve(test_input) == (0, 0)
+
+
+with open("./inputs/09", 'r') as f:
+    lines = f.read()
+
+part1, part2 = solve(lines)
+print(f'Part 1: {part1}, Part 2: {part2}')
--- create_python.ps1
+++ create_python.ps1
@@ -1,10 +1,25 @@
-for ($i = 5; $i -lt 25; $i++) {
+for ($i = 8; $i -lt 25; $i++) {
     $day = $i.ToString().PadLeft(2, "0");
     $template = @'
+def solve(input):
+    s = 0
+    s2 = 0
+
+    return s, s2
+
+
+test_input = """"""
+
+assert solve(test_input) == (0, 0)
+
+
 with open("./inputs/#day#", 'r') as f:
-    lines = f.read().splitlines()
+    lines = f.read()
+
+part1, part2 = solve(lines)
+print(f'Part 1: {part1}, Part 2: {part2}')
 '@.Replace("#day#", $day);
 
     $name = $day + ".py"
-    New-Item -Path ./2023 -Name $name -ItemType "file" -Value $template
+    New-Item -Path ./2024 -Name $name -ItemType "file" -Value $template
 }
